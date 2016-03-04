183e3e7a1dcd574b595c14698a773669e292b9f1
golgistudio <lauriesreynolds@gmail.com>
2016-03-03 19:23:50 -0800

    added put and delete

--- mongo-shopping-list/src/routes/item.js
+++ mongo-shopping-list/src/routes/item.js
@@ -2,20 +2,43 @@ var express = require('express');
 var Item = require('../services/item');
 var router = express.Router();
 
-router.get('/items', function(req, res) {
+function handleGet(req, res) {
     Item.list(function(items) {
         res.json(items);
     }, function(err) {
         res.status(400).json(err);
     });
-});
+};
 
-router.post('/items', function(req, res) {
+function handlePost(req, res) {
     Item.save(req.body.name, function(item) {
+        console.log("handlePost - " + item)
+        res.status(201).json(item);
+    }, function(err) {
+        res. status(400).json(err);
+    });
+};
+
+function handleDelete(req, res) {
+    Item.delete(req.params.id, function(item) {
         res.status(201).json(item);
     }, function(err) {
         res.status(400).json(err);
     });
-});
+}
+
+function handlePut(req, res) {
+    Item.update(req.body.name, req.params.id, function(item) {
+        res.status(201).json(item);
+    }, function(err) {
+        res.status(400).json(err);
+    });
+
+}
+
+router.get('/items', handleGet)
+router.post('/items', handlePost)
+router.delete('/items/:id', handleDelete)
+router.put('/items/:id', handlePut) 
 
 module.exports = router;
--- mongo-shopping-list/src/services/item.js
+++ mongo-shopping-list/src/services/item.js
@@ -19,3 +19,38 @@ exports.list = function(callback, errback) {
         callback(items);
     });
 };
+
+exports.update = function(name, id, callback, errback) {
+    console.log("service update - " + id + ", name : " + name)
+    Item.findById(id, function(err, doc) {
+        if (err) {
+            errback(err);
+            return;
+        }
+        doc.name = name
+        doc.save(function(err) {
+            if (err) {
+                errback(err)
+                return;
+            }
+            callback(doc);
+        })
+    })
+};
+
+exports.delete = function(id, callback, errback) {
+    Item.findById(id, function(err, doc) {
+        if (err) {
+            errback(err);
+            return;
+        }
+        doc.remove(function(err) {
+             if (err) {
+                errback(err)
+                return;
+            }
+            callback(doc);
+        })
+    })
+};
+
